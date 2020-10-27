9a6017e0a52893b1348a9d07019b9a15bc324fba
sin-dorei <228827120@qq.com>
2020-10-27 08:49:41 +0800

    管理员可删除用户

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,6 +1,14 @@
 <div class="list-group-item">
     <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
     <a href="{{ route('users.show', $user) }}">
-      {{ $user->name }}
+        {{ $user->name }}
     </a>
-  </div>
+    @can('destroy', $user)
+        <form action="{{ route('users.destroy', $user) }}" method="post"
+            class="float-right">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+        </form>
+    @endcan
+</div>
