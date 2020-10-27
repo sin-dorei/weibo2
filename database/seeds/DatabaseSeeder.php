26a501db2f5e0e6219d34203b1e4aaceac115b95
sin-dorei <228827120@qq.com>
2020-10-27 20:31:25 +0800

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(StatusSeeder::class);
+        $this->call(FollowerSeeder::class);
 
         Model::reguard();
     }
