d810ed369478689011bbdf28f62a0b0e7ae62e12
sin-dorei <228827120@qq.com>
2020-10-27 21:11:08 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,8 +60,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
     }
 
     public function followers()
@@ -76,7 +79,7 @@ class User extends Authenticatable
 
     public function follow($user_ids)
     {
-        if (! is_array($user_ids)) {
+        if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->sync($user_ids, false);
@@ -84,7 +87,7 @@ class User extends Authenticatable
 
     public function unfollow($user_ids)
     {
-        if (! is_array($user_ids)) {
+        if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->detach($user_ids);
